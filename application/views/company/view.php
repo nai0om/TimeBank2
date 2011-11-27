d348ee9339b79ffeeb1721011df9006299371c12
nai0om <crazy_complete@hotmail.com>
2011-11-28 00:01:13 +0700

    add event history to company/view/profile.

--- application/views/company/view.php
+++ application/views/company/view.php
@@ -8,3 +8,23 @@
     <li>Website: <?= $company->website; ?></li>
     <li>Logo: <img src="<?= url::base().'media/upload/'.$company->logo; ?>" /></li>
 </ul>
+
+
+<br />
+<h2>Event list</h2>
+<br />
+ <table border="1">
+  <tr>
+	<th>name</th>
+	<th>status</th>
+	<th>time_cost</th>
+	<th>show</th>
+  </tr>
+<?php foreach ($company->events->find_all() as $event):?>
+	<tr>
+	<td><?= $event->name ?></td>
+	<td><?= $event->status ?></td>
+	<td><?= $event->time_cost ?></td>
+	<td><?= HTML::anchor('event/view/'.$event->id, 'show'); ?></td>
+	</tr>
+<? endforeach ?>
