fbff0117ee9119960636c43652f0e7a3be71fb05
varanon <nonarav@gmail.com>
2012-06-03 11:02:35 +0700

    update image

---
+++ application/views/user/signup.php
@@ -0,0 +1,16 @@
+﻿<div id="data" class="popup">
+  <div id="main" role="main">
+
+		<ul><li>สมัครสมาชิก</li><li>เข้าสู่ระบบ</li></ul>
+		
+		<div id="content">
+			<h2>Sign up</h2>
+
+				<h3>สมัครเป็นสมาชิกกับ Jitarsabank.org<br><br>
+				<?= HTML::anchor('user/create', 'สมัครสมาชิกอาสา', array('class'=>'button long170')); ?> <span style="color:#0099cc">หรือ</span> <a class="button long170">สมัครสมาชิกองค์กร</a></h3>
+
+		</div>
+		
+  </div>
+ </div>
+ 
