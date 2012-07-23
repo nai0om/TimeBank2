0c56ebfacb1cb728d61a2ab10c4a70589f55fd1d
Pongrob Saisuwan <xinexo@gmail.com>
2012-07-23 13:27:38 +0700

    - added tinymce 3.5.5 - fixed https://github.com/xinexo/TimeBank/issues/11

---
+++ application/classes/phphelp.php
@@ -0,0 +1,36 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+ 
+class phphelp {
+ 
+	public static function startsWith($haystack, $needle)
+	{
+		$length = strlen($needle);
+		return (substr($haystack, 0, $length) === $needle);
+	}
+	
+	public static function endsWith($haystack, $needle)
+	{
+		$length = strlen($needle);
+		if ($length == 0) {
+			return true;
+		}
+	
+		return (substr($haystack, -$length) === $needle);
+	}
+	
+
+	public static function thai_date($time){  
+		$thai_day_arr = Kohana::$config->load('timebank')->get('thai_day_arr'); 
+		$thai_month_arr = Kohana::$config->load('timebank')->get('thai_month_arr');
+		 
+		$thai_date_return="วัน".$thai_day_arr[date("w",$time)];  
+		$thai_date_return.= "ที่ ".date("j",$time);  
+		$thai_date_return.=" เดือน".$thai_month_arr[date("n",$time)];  
+		$thai_date_return.= " พ.ศ.".(date("Yํ",$time)+543);  
+	
+		return $thai_date_return;  
+	} 
+	
+}
+ 
+?>
