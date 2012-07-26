081ba70e25f78e67d4f02fbade6bded5dce016ce
Jitarsabank <jitarsabank@gmail.com>
2012-07-26 15:34:45 +0700

    Noock edits text

--- application/views/event/browse.php
+++ application/views/event/browse.php
@@ -1,4 +1,4 @@
-<div id="tb_browse">
+﻿<div id="tb_browse">
 	<div id="main" role="main">
 		<div id="sitemap">
 			<li>หน้าแรก</li>
@@ -7,7 +7,7 @@
 		</div>
 		<?php include Kohana::find_file('views', 'shared/accountinfo') ?>
 		
-		<h2>หางานอาสา</h2>
+		<h2>ค้นหาภารกิจจิตอาสา</h2>
 		<div id="icon_set">
             <?= HTML::anchor('welcome/timebankhow', '<img src="'.url::base().'media/img/icon_how_it_work.png"/>'); ?>
             <?= HTML::anchor('event/browse', '<img src="'.url::base().'media/img/icon_browse_event.png"/>'); ?>
@@ -16,34 +16,29 @@
 		
 		<?php 
 			echo Form::open('event/search', array ('id' => 'search', 'method' => 'get')); 
-        	echo Form::input('text', 'ใส่ชื่องานอาสา, ชื่อองค์กร, ชื่อสถานที่'); 
-            echo Form::select('jobs', $jobs, '--ประเภทงานอาสา--');
+        	echo Form::input('text', 'ใส่ชื่อภารกิจ, ชื่อองค์กร, ชื่อสถานที่'); 
+            echo Form::select('jobs', $jobs, '--ประเภทภารกิจ--');
             echo Form::select('location_province', $provices, '--จังหวัด--');
-	        echo Form::submit(NULL, 'หางานอาสา', array ('class' => 'mid')); 
-            echo Form::submit('advance', 'ค้นหาแบบละเอียด', array ('class' => 'long')); 
+	        echo Form::submit(NULL, 'ค้นหา', array ('class' => 'mid')); 
+            echo Form::submit('advance', 'ไปค้นหาแบบละเอียด', array ('class' => 'long')); 
 			echo Form::close();
 		?>
-
-		<blockquote>
-			<h2>ปันเวลาช่วยกัน</h2>
-			<h3><span style="text-decoration:underline"><?= HTML::anchor('event/browse', 'ฝากเวลา'); ?></span>   <span style="font-size:20px;">หรือ</span>   <span style="text-decoration:underline"><?= HTML::anchor('event/create', 'สร้างภารกิจจิตอาสา'); ?></span></h3>
-		</blockquote>
 		
-		<h3 class="title">ประเภทงานอาสา</h3>
+		<h3 class="title">ประเภทภารกิจจิตอาสา</h3>
 		<ul class="type">
 			<?php for($i = 1; $i < sizeof($jobs); $i++):	?>
 				<li><?= HTML::anchor('event/search/?job='.$i,  $jobs[$i]) ?> <span>[<?= $jobs_count[$i] ?>]</span></li>
             <?php endfor; ?>
 		</ul>
 
-		 <h3 class="title" style="float:left;">งานอาสามาใหม่</h3>
+		 <h3 class="title" style="float:left;">ภารกิจล่าสุด</h3>
          
 		<img src="<?= url::base() ?>media/img/tb_browse_line.png" style="position:relative;top:20px;"/><?= HTML::anchor('event/search/', 'แสดงเพิ่มเติม', array('class' => 'read_more')) ?>
 		<table cellpadding=0 cellspacing=0>
 			<tr>
-				<th>งานอาสา</th>
-				<th>ต้องการจากอาสา</th>
-				<th>จำนวนรับสมัคร</th>
+				<th>ชื่อภารกิจ</th>
+				<th>ต้องการเวลา (ช.ม./คน) </th>
+				<th>รับจำนวน</th>
 				<th>เปิดรับสมัคร</th>
 				<th>วัน/เวลาทำงาน</th>
 				<th></th>
