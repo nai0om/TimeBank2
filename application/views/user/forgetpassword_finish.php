0def0ef064d91f4d9eaa577e124fd8f5d21dc6c4
Pongrob Saisuwan <xinexo@gmail.com>
2012-07-28 23:04:03 +0700

    - Email notification & email template - Email for signup/forget password - Added forget password

---
+++ application/views/user/forgetpassword_finish.php
@@ -0,0 +1,15 @@
+<div id="data" class="popup">
+  <div id="main" role="main">
+
+		<?php include Kohana::find_file('views', 'shared/accountinfo') ?>
+		
+		<div id="content">
+			<h2>Thank you</h2>
+			<h3>ได้ทำการส่งข้อมูลไปที่ <a style="color:#0099cc"><?= $email ?></a> เรียบร้อยแล้ว<br>
+				<span style="color:#666;">Note : หากคุณไม่ได้รับอีเมล์กรุณาตรวจสอบใน Junk mail ของคุณด้วยนะคะ</span>
+			</h3>
+		</div>
+		
+  </div>
+ </div>
+ 
