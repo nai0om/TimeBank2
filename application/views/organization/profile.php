e8455b0db4c9951af051d0373ffd44380eaa94ab
Pongrob Saisuwan <xinexo@gmail.com>
2012-08-01 14:13:42 +0700

    tinymce in organization profile editor

--- application/views/organization/profile.php
+++ application/views/organization/profile.php
@@ -66,7 +66,7 @@
                 </p>
 				<p><label>ลักษณะกิจกรรมขององค์กร</label></p>
 				<p>
-					<?= Form::input('activity', HTML::chars($organization->activity), array('class'=>'long')); ?>
+					<?= Form::textarea('activity', HTML::chars($organization->activity), array('class'=>'long')); ?>
                     <div class="error">
                         <?= Arr::get($errors, 'activity'); ?>
                     </div>
@@ -154,3 +154,23 @@
   </div>
 </div>
 
+<!-- TinyMCE -->
+<script type="text/javascript" src="<?= url::base(); ?>media/js/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
+<script type="text/javascript">
+tinyMCE.init({
+        mode : "textareas",
+        theme : "advanced",
+        plugins : "emotions,spellchecker,advhr,insertdatetime", 
+       
+		theme_advanced_resizing_max_width: "400",
+        // Theme options - button# indicated the row# only
+        theme_advanced_buttons1 : "justifyleft,justifycenter,justifyright,fontselect,fontsizeselect,formatselect",
+		theme_advanced_buttons2 : "bold,italic,underline,|,bullist,numlist,|,link,unlink,anchor,image",
+		theme_advanced_buttons3 : "cut,copy,paste,|,outdent,indent,|,undo,redo,|,code,preview,|,forecolor,backcolor",        theme_advanced_buttons4: "",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true,
+});
+</script>
+<!-- /TinyMCE -->
