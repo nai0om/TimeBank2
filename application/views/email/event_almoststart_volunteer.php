960389413baea67a4d2a78c48c46ca739495937c
Pongrob Saisuwan <xinexo@gmail.com>
2012-08-10 16:52:50 +0700

    fixed #114

---
+++ application/views/email/event_almoststart_volunteer.php
@@ -0,0 +1,103 @@
+﻿<style type="text/css">h1 { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 38px; color: #212121 !important; margin: 0; padding: 0 0 0 15px; font-style: italic; }
+
+h2 { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 20px; color: #212121 !important; margin: 0 0 7px 0; }
+
+p { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 16px; line-height: 21px; color: #212121; margin: 0 0 12px 0; }
+
+a{ font-family:Verdana, Arial, Helvetica, sans-serif;color:#0099cc; text-decoration:underline;}</style></head>
+
+<body style="margin:0; padding:0;">
+
+<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+
+    <tr>
+
+        <td valign="top" align="center">
+
+            <table width="565" border="0" cellpadding="0" cellspacing="0" align="center" style="">
+
+                <tr>
+
+                    <td align="center" valign="top" style="padding-top:10px;">
+
+                        <table class="top" width="100%" border="0" cellspacing="0" cellpadding="0">
+
+                            <tr>
+
+                                <td valign="top" height="121" class="header">
+
+                                    <img src="<?= $vars['urlbase'].'media/upload/email/header.jpg' ?>" style="display:block;float:none;">
+
+                                </td>
+
+                            </tr>
+
+                        </table>
+
+                    </td>
+
+                </tr>
+
+                <tr>
+
+                    <td align="center" valign="top" bgcolor="#e7daca">
+
+						 <table class="top" width="90%" border="0" cellspacing="0" cellpadding="0">
+
+                            <tr>
+
+                                <td valign="top" class="header">
+
+                                    <p>Hello, <?= $vars['displayname'] ?></p>
+
+									<p><img src="<?= $vars['urlbase'].'media/upload/email/bullet.png' ?>">งานอาสาที่คุณสมัครไป ใกล้ถึงวันจัดกิจกรรม<br>
+
+									<a href="<?= $vars['urlbase'].'event/view/'.$vars['event_id'] ?>"><?= $vars['event_name'] ?></a></p>								 
+
+									<br clear="left">
+
+									<br clear="left">
+
+									<p>ขอบคุณ</p>
+
+									<p>Jitarsabank.org</p>
+
+                                </td>
+
+                            </tr>
+
+                        </table>
+
+                    </td>
+
+                </tr>
+
+				<tr>
+
+                    <td align="center" valign="top">
+
+                        <table class="top" width="100%" border="0" cellspacing="0" cellpadding="0">
+
+                            <tr>
+
+                                <td valign="top" height="121" class="footer">
+
+                                </td>
+
+                            </tr>
+
+                        </table>
+
+                    </td>
+
+                </tr>
+
+            </table>
+
+        </td>
+
+    </tr>
+
+</table>
+
+</body>
