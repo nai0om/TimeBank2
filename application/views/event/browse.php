3bb9b5d802b2ae511233e9e480f88636a36b5075
naioom.th <crazy_complete@hotmail.com>
2012-08-14 23:53:29 +0700

    issue #149

--- application/views/event/browse.php
+++ application/views/event/browse.php
@@ -16,7 +16,7 @@
 		
 		<?php 
 			echo Form::open('event/search', array ('id' => 'search', 'method' => 'get')); 
-        	echo Form::input('text', 'ใส่ชื่อภารกิจ, ชื่อองค์กร, ชื่อสถานที่'); 
+        	echo Form::input('query', 'ใส่ชื่อภารกิจ, ชื่อองค์กร, ชื่อสถานที่'); 
             echo Form::select('jobs', $jobs, '--ประเภทภารกิจ--');
             echo Form::select('location_province', $provices, '--จังหวัด--');
 	        echo Form::submit(NULL, 'ค้นหา', array ('class' => 'mid')); 
