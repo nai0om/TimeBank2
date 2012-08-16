018d240bde4c025afdf30792d2abe3e4c65837ee
naioom.th <crazy_complete@hotmail.com>
2012-08-17 02:14:55 +0700

    issue #80

--- application/views/event/edit.php
+++ application/views/event/edit.php
@@ -10,7 +10,8 @@
 		<h2>แก้ไขงานอาสา</h2>
 		<?= Form::open('event/edit/'.$event->id, array('enctype' => 'multipart/form-data')); ?>
 			<?php include Kohana::find_file('views', 'event/form') ?>
-			<input type="submit" value="บันทึกข้อมูล">
+			<input type="submit" value="บันทึกข้อมูล" 
+            onclick="alert('อาสาที่สมัครร่วมกิจกรรมอาจจะไม่รับทราบการเปลี่ยนแปลง กรุณาแจ้งให้อาสาของท่านทราบด้วยตนเอง หากการเปลี่ยนแปลงนั้นสำคัญ เช่น วัน-เวลาจัดกิจกรรม, สถานที่ ฯลฯ')">
 			</div>
 		<?= Form::close(); ?>
 		
