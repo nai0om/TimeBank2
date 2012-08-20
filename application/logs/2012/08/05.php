0aecf06cf7057a2f6f12b0ed2c34ca8790bde38f
naioom.th <crazy_complete@hotmail.com>
2012-08-20 23:26:17 +0700

    move foooter

---
+++ application/logs/2012/08/05.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-08-05 21:51:31 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/events/501599a776dc320120725-3-1[1].jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-08-05 21:51:31 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/events/501599a776dc320120725-3-1[1].jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /Applications/MAMP/htdocs/TimeBank/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-08-05 21:51:34 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/events/501599a776dc320120725-3-1[1].jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-08-05 21:51:34 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/events/501599a776dc320120725-3-1[1].jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /Applications/MAMP/htdocs/TimeBank/index.php(109): Kohana_Request->execute()
+#1 {main}
