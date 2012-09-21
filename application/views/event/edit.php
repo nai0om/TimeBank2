660084e9e08a7f34fed5d8084dd24267d2c269a7
Orawee Hinthong <orawee.hinthong@gmail.com>
2012-09-21 07:20:40 -0700

    #278

--- application/views/event/edit.php
+++ application/views/event/edit.php
@@ -10,9 +10,10 @@
 		<h2>แก้ไขงานอาสา</h2>
 		<?= Form::open('event/edit/'.$event->id, array('enctype' => 'multipart/form-data')); ?>
 			<?php include Kohana::find_file('views', 'event/form') ?>
-			<input type="submit" value="บันทึกข้อมูล" 
-            onclick="alert('อาสาที่สมัครร่วมกิจกรรมอาจจะไม่รับทราบการเปลี่ยนแปลง กรุณาแจ้งให้อาสาของท่านทราบด้วยตนเอง หากการเปลี่ยนแปลงนั้นสำคัญ เช่น วัน-เวลาจัดกิจกรรม, สถานที่ ฯลฯ')">
 			</div>
+			<div style="clear:both"></div>
+			<input style="margin:0 auto;" type="submit" value="บันทึกข้อมูล" 
+            onclick="alert('อาสาที่สมัครร่วมกิจกรรมอาจจะไม่รับทราบการเปลี่ยนแปลง กรุณาแจ้งให้อาสาของท่านทราบด้วยตนเอง หากการเปลี่ยนแปลงนั้นสำคัญ เช่น วัน-เวลาจัดกิจกรรม, สถานที่ ฯลฯ')">
 		<?= Form::close(); ?>
 		
 <?php include Kohana::find_file('views', 'shared/footer') ?>
