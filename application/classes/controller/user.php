2d71c461c6d6ea61571b66a05c39c1fd38bdad7a
naioom.th <crazy_complete@hotmail.com>
2012-10-22 21:23:58 +0700

    #372

--- application/classes/controller/user.php
+++ application/classes/controller/user.php
@@ -490,6 +490,9 @@ class Controller_User extends Controller_Template {
 			$this->user->nickname = Arr::get($_POST, 'nickname');
 			$this->user->first_name = Arr::get($_POST, 'first_name');
 			$this->user->last_name = Arr::get($_POST, 'last_name');
+			
+			if (Arr::get($_POST, 'phone') == '')
+				 $errors['phone'] = __('phone much not empty.');
 			$this->user->phone = Arr::get($_POST, 'phone');
 			$year = Arr::get($_POST, 'year') - 543;
 			$month = Arr::get($_POST, 'month');
