2dbdf14decd44c658d52a7d23b07ed79355fdad6
naioom.th <crazy_complete@hotmail.com>
2012-10-24 22:53:55 +0700

    #375

--- application/views/organization/view.php
+++ application/views/organization/view.php
@@ -63,7 +63,7 @@
 			<h4>อีเมล</h4><span><?= $org_user->email ?></span>
 			<h4>ติดต่อ </h4><span><?= $organization->contactperson ?></span>
 			<h4>เว็บไซต์</h4><span><a href="<?= $organization->website ?>" target="_blank"><?= $organization->website ?></a></span>
-			<h4>เฟสบุ๊ค</h4><span><a href="<?= 'http://'.$organization->facebook ?>" target="_blank"><?= $organization->facebook ?></a></span>
+			<h4>เฟสบุ๊ค</h4><span><a href="<?= $organization->facebook ?>" target="_blank"><?= $organization->facebook ?></a></span>
 			<h4>ทวิตเตอร์</h4><span><a href="<?='http://www.twitter.com/'.$organization->twitter ?>" target="_blank"><?= ($organization->twitter == '' )?  '': '@'.$organization->twitter ?></a></span>
 		</div>
 
