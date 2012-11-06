32049667204ee8ad33fb03990f4449b2ea7e9b1c
naioom.th <crazy_complete@hotmail.com>
2012-11-06 23:03:08 +0700

    #321

--- application/views/template.php
+++ application/views/template.php
@@ -38,8 +38,8 @@
   <script type="text/javascript" src="<?= url::base(); ?>media/widget/lib/jquery.ui.core.js"></script>
   <script type="text/javascript" src="<?= url::base(); ?>media/widget/lib/jquery.ui.widget.js"></script>
   <script type="text/javascript" src="<?= url::base(); ?>media/widget/lib/jquery.ui.rcarousel.js"></script>
-  <script type="text/javascript" src="<?= url::base(); ?>media/js/jquery-ui-timepicker-addon.js"></script>
   <script type="text/javascript" src="<?= url::base(); ?>media/js/jquery.ui.datepicker-th.js"></script>
+  <script type="text/javascript" src="<?= url::base(); ?>media/js/jquery.ui.datepicker.ext.be.js"></script>
   <script type="text/javascript" src="<?= url::base(); ?>media/js/jquery.tokeninput.js"></script>
   <!-- cript type="text/javascript" src="widget/lib/jquery.ui.rcarousel.js"></script -->
 	
