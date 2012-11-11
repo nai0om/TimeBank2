30bb8cfc4f47e510f0cd3680ce9e838936c4562f
naioom.th <crazy_complete@hotmail.com>
2012-11-11 14:27:40 +0700

    #386

--- application/views/admin/training/training.php
+++ application/views/admin/training/training.php
@@ -9,11 +9,6 @@
     <th >id</th>		 	 		 	 		
     <th >ชื่องาน</th>	 	 	 	 	 	 		 
     <th >วันที่</th>		 	 	 					 
-    <th >รายละเอียด</th>			 	 	 					 
-    <th >รูปหลัก</th>			 	 	 	 	 	 		 
-    <th >รูปหน้า index</th>		 	 	 					 
-   		 	 	 					 
-    <th >วิดีโอ</th>			 	 	 					 
     <th >วันที่สร้าง</th>
     <th>แก้ไข</th>
     <th>ลบ</th>
@@ -23,10 +18,6 @@
         <td ><?=  $training->id; ?></td>
         <td ><?= HTML::anchor('training/view/'.$training->id, '<strong>'.$training->topic.'<? ?></strong>'); ?></td>
         <td ><?=  $training->date_message; ?></td>
-        <td ><?=  $training->message; ?></td>
-        <td ><?=  $training->main_pic; ?></td>
-        <td ><?=  $training->thm_pic; ?></td>   
-        <td ><?=  $training->video; ?></td>
         <td ><?=  $training->timestamp; ?></td>
         <td><?= HTML::anchor('admin/edittraining/'.$training->id, '<strong>แก้ไข</strong>'); ?></td> 
         <td><?= HTML::anchor('admin/deletetraining/'.$training->id, '<strong>ลบ</strong>'); ?> </td>
