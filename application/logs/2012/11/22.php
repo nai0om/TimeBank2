47b9086711251ebbfd37f85986d96c2aabeccd64
naioom.th <crazy_complete@hotmail.com>
2012-11-22 02:07:29 +0700

    #353

---
+++ application/logs/2012/11/22.php
@@ -0,0 +1,32 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-11-22 02:06:18 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/volunteers/501600a0056fepig-4.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-11-22 02:06:18 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/volunteers/50165a166d381Noock.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-11-22 02:06:18 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/volunteers/50165a166d381Noock.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /Applications/MAMP/htdocs/TimeBank/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-11-22 02:06:18 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/volunteers/501600a0056fepig-4.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /Applications/MAMP/htdocs/TimeBank/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-11-22 02:06:18 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/volunteers/501600a0056fepig-4.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-11-22 02:06:18 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/volunteers/501600a0056fepig-4.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /Applications/MAMP/htdocs/TimeBank/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-11-22 02:06:18 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/volunteers/50165a166d381Noock.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-11-22 02:06:18 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/volunteers/50165a166d381Noock.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /Applications/MAMP/htdocs/TimeBank/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-11-22 02:06:21 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/organizations/5055719a08127WP_000452.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-11-22 02:06:21 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/organizations/5055719a08127WP_000452.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /Applications/MAMP/htdocs/TimeBank/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-11-22 02:06:21 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/organizations/5055719a08127WP_000452.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-11-22 02:06:21 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/upload/organizations/5055719a08127WP_000452.jpg ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /Applications/MAMP/htdocs/TimeBank/index.php(109): Kohana_Request->execute()
+#1 {main}
