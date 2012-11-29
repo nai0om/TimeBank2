685566d4d65264c42714d6525b1373c5c5605482
Jitarsabank <jitarsabank@gmail.com>
2012-11-29 11:32:25 +0700

    Noock edited text

--- application/views/admin/partner/form.php
+++ application/views/admin/partner/form.php
@@ -1,7 +1,7 @@
-    <label>ชื่อ : </label><?= Form::input('alias', '', array('style' => 'width: 400px')); ?>
+    <label>ชื่อภาคี : </label><?= Form::input('alias', '', array('style' => 'width: 400px')); ?>
    <br />
     <label>เว็บไซต์ : </label><?= Form::input('link' , '', array('style' => 'width: 400px')); ?>
     <br />
-    <label>โลโก้ภาคี</label><?= Form::file('image', array('style' => 'width: 400px')); ?>
+    <label>โลโก้</label><?= Form::file('image', array('style' => 'width: 400px')); ?>
   	<br />
   
