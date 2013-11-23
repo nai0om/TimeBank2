76caf358d83437a9a6991febfa253016a487b788
Pariyawit <pariyawit.jat@gmail.com>
2013-11-23 23:53:13 +0700

    edit nav bar (event/view) #212 edit 'close regis' button #414

--- application/views/event/view.php
+++ application/views/event/view.php
@@ -13,9 +13,9 @@
 <? endif ?>
   <div id="main" role="main" >
 		<div id="sitemap">
-			<li>หน้าแรก</li>
-			<li>ใช้บริการธนาคารจิตอาสา</li>
-			<li>ดูภารกิจจิตอาสา</li>
+			<li><?= HTML::anchor('welcome/home', 'หน้าแรก') ?></li>
+            <li><?= HTML::anchor('welcome/timebank', 'ใช้บริการธนาคารจิตอาสา') ?></li>
+            <li><?= HTML::anchor('event/browse', 'ดูภารกิจจิตอาสา') ?></li>
 			<li><?= $event->name ?></li>
 		</div>
 		
